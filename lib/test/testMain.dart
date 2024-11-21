df3695d0762b7c2f93fb1287a30fee3daa5ebf9d
Apricityx <apricityx@qq.com>
2024-11-21 12:13:51 +0800

    Dev. Test page.

---
+++ lib/test/testMain.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:paddle_score_app/test/testPage.dart';
+
+void main() {
+  // 初始化数据库工厂
+  databaseFactory = databaseFactoryFfi;
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    print("Learning widget built");
+    return MaterialApp(
+        title: 'PaddleScoreApp 演示',
+        debugShowCheckedModeBanner: false, // 取消右上角的DEBUG标记
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
+        ),
+        home: TestPage());
+  }
+}
---
+++ lib/test/testPage.dart
@@ -0,0 +1,163 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:paddle_score_app/DataHelper.dart';
+import 'package:paddle_score_app/test/testUnit.dart';
+
+import 'package:paddle_score_app/utils/GlobalFunction.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:paddle_score_app/utils/DatabaseManager.dart';
+
+class TestPage extends StatelessWidget {
+  const TestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+    // delete test code -todo
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('开发者模式'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              print(1); // 点击加号时输出1
+            },
+          ),
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          TestUnit(text: "导入报名表", icon: Icons.upload_file, callBack: ()async{
+            String dbName = "athlete";
+            File xlsxFile = File("/home/apricityx/Desktop/参赛信息-_汉丰湖.xlsx");
+
+            await DataHelper.loadExcelFileToAthleteDatabase(
+                dbName, xlsxFile.readAsBytesSync());
+
+          }),
+          TestUnit(text: "下载长距离空成绩表", icon: Icons.sim_card_download, callBack: ()async{
+            String dbName = "athlete";
+            List<int>? excelFileBytes =
+            await DataHelper.generateLongDistanceScoreExcel(dbName);
+            if (excelFileBytes == null) {
+              throw Exception("生成 Excel 文件失败");
+            }
+            // String? filePath = await FilePicker.platform.saveFile(
+            //   dialogTitle: '保存 Excel 文件',
+            //   fileName: '长距离成绩单.xlsx',
+            // );
+            var filePath =
+                "/run/media/apricityx/Data1/Desktop/长距离成绩单.xlsx"; // -todo
+            // 创建文件并写入字节数据
+            File file = File(filePath);
+            await file.writeAsBytes(excelFileBytes);
+
+            print('文件已保存到: $filePath');
+            // 关闭对话框
+
+          }),
+          TestUnit(
+              text: "导入已录入的长距离成绩",
+              icon: Icons.import_contacts,
+              callBack: () async {
+                File exampleFile = File("/home/apricityx/Desktop/长距离成绩单.xlsx");
+                await DataHelper.importLongDistanceScore(
+                    'athlete', exampleFile.readAsBytesSync());
+              }),
+          const Divider(),
+          TestUnit(
+              text: '下载初赛空成绩表',
+              icon: Icons.sim_card_download_outlined,
+              callBack: () async {
+                String dbName = "athlete";
+                String path = "/home/apricityx/Desktop/大师组男子竞速初赛成绩表.xlsx";
+                List<int>? exampleFile = await DataHelper.generateGenericExcel(
+                    "大师组男子", CType.sprint, SType.firstRound, dbName);
+                File file = File(path);
+                await file.writeAsBytes(exampleFile!);
+                print('文件已保存到: $path');
+              }),
+          TestUnit(
+              text: '导入已填写的初赛成绩表',
+              icon: Icons.import_contacts,
+              callBack: () async {
+                String dbName = "athlete";
+                String path = "/home/apricityx/Desktop/大师组男子竞速初赛成绩表.xlsx";
+                await DataHelper.importGenericCompetitionScore(
+                    "大师组男子",
+                    File(path).readAsBytesSync(),
+                    CType.sprint,
+                    SType.firstRound,
+                    dbName);
+              }),
+          const Divider(),
+          TestUnit(
+              text: '下载1/2决赛空成绩表',
+              icon: Icons.sim_card_download_outlined,
+              callBack: () async {
+                String dbName = "athlete";
+                String path = "/home/apricityx/Desktop/大师组男子竞速二分之一决赛成绩表.xlsx";
+                List<int>? exampleFile = await DataHelper.generateGenericExcel(
+                    "大师组男子", CType.sprint, SType.semifinals, dbName);
+                File file = File(path);
+                await file.writeAsBytes(exampleFile!);
+                print('文件已保存到: $path');
+              }),
+          TestUnit(
+              text: '导入已完成的1/2决赛成绩表',
+              icon: Icons.import_contacts,
+              callBack: () async {
+                String dbName = "athlete";
+                String path = "/home/apricityx/Desktop/大师组男子竞速二分之一决赛成绩表.xlsx";
+                await DataHelper.importGenericCompetitionScore(
+                    "大师组男子",
+                    File(path).readAsBytesSync(),
+                    CType.sprint,
+                    SType.semifinals,
+                    dbName);
+              }),
+          const Divider(),
+          TestUnit(
+              text: '下载决赛空成绩表',
+              icon: Icons.sim_card_download_outlined,
+              callBack: () async {
+                String dbName = "athlete";
+                String path = "/home/apricityx/Desktop/大师组男子竞速决赛成绩表.xlsx";
+                List<int>? exampleFile = await DataHelper.generateGenericExcel(
+                    "大师组男子", CType.sprint, SType.finals, dbName);
+                File file = File(path);
+                await file.writeAsBytes(exampleFile!);
+                print('文件已保存到: $path');
+              }),
+          TestUnit(
+              text: '导入已完成的决赛成绩表',
+              icon: Icons.import_contacts,
+              callBack: () async {
+                String dbName = "athlete";
+                String path = "/home/apricityx/Desktop/大师组男子竞速决赛成绩表.xlsx";
+                await DataHelper.importGenericCompetitionScore(
+                    "大师组男子",
+                    File(path).readAsBytesSync(),
+                    CType.sprint,
+                    SType.finals,
+                    dbName);
+              }),
+          TestUnit(
+            text: '测试实例',
+            icon: Icons.running_with_errors,
+            callBack: () async {
+              var a = await getDivisions("athlete");
+              // 等待5秒
+              await Future.delayed(Duration(seconds: 5));
+              print(a);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
